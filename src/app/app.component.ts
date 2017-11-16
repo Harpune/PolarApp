eacc6ffe41c6ef2ce59df6f12baf63b8610502ab
Lukas Hennig <lukas.hennig@gmx.de>
2017-11-16 13:43:08 +0100

    Storage SQLite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 
-import {Nav, Platform} from 'ionic-angular';
+import {Platform} from 'ionic-angular';
 
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
+import {SQLite, SQLiteObject} from "@ionic-native/sqlite";
+import {SQLitePorter} from '@ionic-native/sqlite-porter';
 
 import {LoginPage} from "../pages/login/login";
 import {TabsPage} from "../pages/tabs/tabs";
@@ -14,14 +16,19 @@ import {TabsPage} from "../pages/tabs/tabs";
 export class MyApp {
   pages: Array<{ title: string, icon: string, component: any }>;
   rootPage: any = LoginPage;
+  token: any;
 
   constructor(private platform: Platform,
               private statusBar: StatusBar,
-              private splashScreen: SplashScreen,) {
+              private splashScreen: SplashScreen,
+              private sqlite: SQLite,
+              private sqlitePorter: SQLitePorter) {
     //localStorage.removeItem('currentUser');
-    let token = JSON.parse(localStorage.getItem('currentUser'));
-    console.log('User logged in ', token);
-    if (token) {
+    this.token = JSON.parse(localStorage.getItem('currentUser'));
+    let user = JSON.parse(localStorage.getItem('user'));
+    console.log('Token logged in ', this.token);
+    console.log('User logged in ', user);
+    if (this.token) {
       this.rootPage = TabsPage;
     } else {
       this.rootPage = LoginPage;
@@ -31,6 +38,28 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  ngOnInit(): void {
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      this.createTables(db)
+    })
+  }
+
+  createTables(db: any) {
+    this.sqlitePorter.importJsonToDb(db._objectInstance, this.token).then(success => {
+        console.log("SQLite", success)
+      }, error => {
+        console.log("SQLLite", error)
+      }
+    );
+
+    this.sqlitePorter.exportDbToJson(db).then(data => {
+      console.log("SQLite data", data)
+    })
   }
 
 }
