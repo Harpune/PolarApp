2a951dcb9b33626118b5ce8e42972d6a7f109632
Lukas Hennig <lukas.hennig@gmx.de>
2018-05-01 12:33:45 +0200

    - Popover updated

--- src/pages/popover/popover.ts
+++ src/pages/popover/popover.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavParams, ViewController} from "ionic-angular";
+import {AlertController, App, Events, NavController, NavParams, ViewController} from "ionic-angular";
 import {LocalDataProvider} from "../../providers/local-data/local-data";
 
 @Component({
@@ -13,10 +13,14 @@ import {LocalDataProvider} from "../../providers/local-data/local-data";
   `
 })
 export class PopoverPage {
-  data:any;
+  data: any;
 
   constructor(private viewCtrl: ViewController,
+              private navCtrl: NavController,
+              private alertCtrl: AlertController,
               private navParams: NavParams,
+              private events: Events,
+              private app: App,
               private localData: LocalDataProvider) {
     this.data = navParams.data;
     console.log('popover', this.data);
@@ -27,9 +31,62 @@ export class PopoverPage {
   }
 
   remove() {
-    close();
-    this.localData.delete(this.data.data, this.data.type);
     console.log('Popover', 'delete');
+    this.close();
+
+    let stringType = "dieses Ereignis";
+
+    switch (this.data['type']['id']) {
+      case 0:
+        stringType = "dieses Training";
+        break;
+      case 1:
+        stringType = "dieses Aktivität";
+        break;
+      case 2:
+        stringType = "diese Information";
+        break;
+      default:
+
+    }
+
+
+    this.alertCtrl.create({
+      title: 'Löschen?',
+      message: `Wollen Sie ${stringType} wirklich löschen? Das kann nicht rückgängig gemacht werden!`,
+      buttons: [
+        {
+          text: 'Nein',
+          role: 'cancel',
+          handler: () => {
+            console.log('Delete', this.data['type'], 'Cancel clicked');
+          }
+        }, {
+          text: 'Ja',
+          handler: () => {
+            console.log('Delete', this.data['type'], 'Ok clicked');
+            this.localData.delete(this.data.data, this.data.type).then(success => {
+              switch (this.data['type']['id']) {
+                case 0:
+                  this.events.publish('exercise:data', true);
+                  break;
+                case 1:
+                  this.events.publish('activity:data', true);
+                  break;
+                case 2:
+                  this.events.publish('physical:data', true);
+                  break;
+              }
+
+              this.app.getRootNav().pop();
+              console.log('Delete Activity', 'Success', success);
+            }, error => {
+              console.log('Delete Activity', 'Error', error);
+            });
+          }
+        }
+      ]
+    }).present();
   }
 
 }
